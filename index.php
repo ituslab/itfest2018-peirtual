b1dfe79e15c65b42728888374147b7c2f4fe32f7
Sutan Nst <sgnzst@gmail.com>
2018-11-05 19:52:40 +0700

    modified index and .htaccess

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
+<?php require_once('core/init.php') ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/bulma.min.css">
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/css/bulma.min.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
     <title></title>
   </head>
   <body>
     
-    <script type="text/javascript" src="assets/js/main.js"></script>
+    <script type="text/javascript" src="/assets/js/main.js"></script>
   </body>
 </html>
