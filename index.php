f62450bc5950ff7406a21e9a40b8e6508fdb041f
Refnaldy <irvanrefnaldy98@gmail.com>
2018-11-06 09:59:43 +0700

    modified index & add Sponso Logo

--- index.php
+++ index.php
@@ -230,59 +230,41 @@
     </div>
   </section>
   <footer class="footer">
-    <div class="container">
-      <div class="columns">
-        <div class="column is-3 is-offset-2">
-          <h2><strong>Category</strong></h2>
-          <ul>
-            <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-            <li><a href="#">Vestibulum errato isse</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-            <li><a href="#">Aisia caisia</a></li>
-            <li><a href="#">Murphy's law</a></li>
-            <li><a href="#">Flimsy Lavenrock</a></li>
-            <li><a href="#">Maven Mousie Lavender</a></li>
-          </ul>
-        </div>
-        <div class="column is-3">
-          <h2><strong>Category</strong></h2>
-          <ul>
-            <li><a href="#">Labore et dolore magna aliqua</a></li>
-            <li><a href="#">Kanban airis sum eschelor</a></li>
-            <li><a href="#">Modular modern free</a></li>
-            <li><a href="#">The king of clubs</a></li>
-            <li><a href="#">The Discovery Dissipation</a></li>
-            <li><a href="#">Course Correction</a></li>
-            <li><a href="#">Better Angels</a></li>
-          </ul>
-        </div>
-        <div class="column is-4">
-          <h2><strong>Category</strong></h2>
-          <ul>
-            <li><a href="#">Objects in space</a></li>
-            <li><a href="#">Playing cards with coyote</a></li>
-            <li><a href="#">Goodbye Yellow Brick Road</a></li>
-            <li><a href="#">The Garden of Forking Paths</a></li>
-            <li><a href="#">Future Shock</a></li>
-          </ul>
-        </div>
+    <div class="content has-text-centered"><h2>Sponsor</h2></div>
+    <div class="columns is-mobile">
+      <div class="column">
+        <img src="https://itpolsri.com/Images/GlobalFM.jpeg" width="112" height="28" alt="GlobalFM">
       </div>
-      <div class="content has-text-centered">
-        <p>
-          <a class="icon" href="https://github.com/dansup/bulma-templates">
-            <i class="fa fa-github"></i>
-          </a>
-        </p>
-        <div class="control level-item">
-          <a href="https://github.com/dansup/bulma-templates">
-            <div class="tags has-addons">
-              <span class="tag is-dark">Bulma Templates</span>
-              <span class="tag is-info">MIT license</span>
-            </div>
-          </a>
-        </div>
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SponsorGamerINA.jpeg" alt="GamerINA">
       </div>
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SponsorGoodNews.jpeg"  alt="GoodNews">
+      </div>
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SponsorMomea.jpeg"  alt="Momea">
+      </div>
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SponsorPalembangEvents.jpeg"  alt="Palembang Events">
+      </div>
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SponsorJon.jpeg"  alt="Jon">
+      </div>
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SriwijayaPost.jpeg"  alt="Sriwijaya Post">
+      </div>
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SponsorIdea.jpeg"  alt="Sponsor Idea">
+      </div>       
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SponsorElti.jpeg"  alt="Sponsor Elti">
+      </div>   
+      <div class="column">
+        <img src="https://itpolsri.com/Images/SriwijayaHost.jpeg" alt="Sriwijaya Host">
+      </div>   
+      
     </div>
+    
   </footer>
   <script type="text/javascript" src="/assets/js/main.js"></script>
 </body>
