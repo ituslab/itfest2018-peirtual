e8e3b56548133e526f8b2eb15069029d5c618c10
Sutan Nst <sgnzst@gmail.com>
2018-11-07 16:12:57 +0700

    cut navbar

---
+++ views/desktop/includes/navbar.php
@@ -0,0 +1,15 @@
+<nav id="nav" role="navigation">
+  <div class="nav-wrapper container">
+    <a href="#" class="brand-logo">E-Perpus</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="<?= base_url() ?>login">Login</a></li>
+      <li><a href="<?= base_url() ?>register">Register</a></li>
+    </ul>
+
+    <ul id="nav-mobile" class="sidenav">
+      <li><a href="#">Login</a></li>
+      <li><a href="">Register</a></li>
+    </ul>
+    <a id="nav-trigger" href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+  </div>
+</nav>
--- views/desktop/landing.php
+++ views/desktop/landing.php
@@ -7,22 +7,7 @@
 </head>
 
 <body>
-  <nav id="nav" role="navigation">
-    <div class="nav-wrapper container">
-      <a href="#" class="brand-logo">E-Perpus</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="<?= base_url() ?>login">Login</a></li>
-        <li><a href="<?= base_url() ?>register">Register</a></li>
-      </ul>
-
-      <ul id="nav-mobile" class="sidenav">
-        <li><a href="#">Login</a></li>
-        <li><a href="">Register</a></li>
-      </ul>
-      <a id="nav-trigger" href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
-
+  <?php include_once 'includes/navbar.php' ?>
   <div id="index-banner" class="parallax-container landing-parallax">
     <div class="section no-pad-bot">
       <div class="container">
