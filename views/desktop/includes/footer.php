04b0855476e701375061ce68bdb604ab011e439d
Refnaldy <irvanrefnaldy98@gmail.com>
2018-11-08 07:28:30 +0700

    modified Footer & css Mobile

--- views/desktop/includes/footer.php
+++ views/desktop/includes/footer.php
@@ -1,33 +1,21 @@
-<footer class="page-footer">
+<footer class="page-footer does-footer">
   <div class="container">
     <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">Company Bio</h5>
-        <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+      <div class="col s12">
+        <h3><strong>Sponsor</strong></h3>
       </div>
-      <div class="col l3 s12">
-        <h5 class="white-text">Settings</h5>
-        <ul>
-          <li><a class="white-text" href="#!">Link 1</a></li>
-          <li><a class="white-text" href="#!">Link 2</a></li>
-          <li><a class="white-text" href="#!">Link 3</a></li>
-          <li><a class="white-text" href="#!">Link 4</a></li>
-        </ul>
+      <div class="col s12">
+        <img src="https://itpolsri.com/Images/GlobalFM.jpeg"  class="responsive-img" alt="GlobalFM">
+        <img src="https://itpolsri.com/Images/SponsorGamerINA.jpeg" class="responsive-img" alt="GamerINA">
+        <img src="https://itpolsri.com/Images/SponsorGoodNews.jpeg"  class="responsive-img" alt="GoodNews">
+        <img src="https://itpolsri.com/Images/SponsorMomea.jpeg"  class="responsive-img" alt="Momea">
+        <img src="https://itpolsri.com/Images/SponsorPalembangEvents.jpeg"  class="responsive-img" alt="Palembang Events">
+        <img src="https://itpolsri.com/Images/SponsorJon.jpeg"  class="responsive-img" alt="Jon">
+        <img src="https://itpolsri.com/Images/SriwijayaPost.jpeg"  class="responsive-img" alt="Sriwijaya Post">
+        <img src="https://itpolsri.com/Images/SponsorElti.jpeg"  class="responsive-img" alt="Sponsor Elti">
+        <img src="https://itpolsri.com/Images/SriwijayaHost.jpeg" class="responsive-img" alt="Sriwijaya Host">
+        <img src="https://itpolsri.com/Images/SponsorIdea.jpeg" class="responsive-img"  alt="Sponsor Idea">
       </div>
-      <div class="col l3 s12">
-        <h5 class="white-text">Connect</h5>
-        <ul>
-          <li><a class="white-text" href="#!">Link 1</a></li>
-          <li><a class="white-text" href="#!">Link 2</a></li>
-          <li><a class="white-text" href="#!">Link 3</a></li>
-          <li><a class="white-text" href="#!">Link 4</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="footer-copyright">
-    <div class="container">
-      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
     </div>
   </div>
 </footer>
--- views/mobile/js/app.js
+++ views/mobile/js/app.js
@@ -1,6 +1,15 @@
 $(function(){
-  // Initialization code
-  $('ons-button').on('click', function(e) {
-    ons.notification.alert('Button is tapped!');
-  })
+  // $('#prof').on('click',function(e){
+  //   ons.notification.toast("you have new Message!", {timeout : 1500, animation : "fall"});
+  // });
 });
+
+window.fn = {}
+
+window.fn.click = function(){
+  alert("Dasboard")
+}
+window.fn.but = function(){
+  ons.notification.toast("you have new Message!", {timeout : 1500, animation : "fall"});
+}
+
