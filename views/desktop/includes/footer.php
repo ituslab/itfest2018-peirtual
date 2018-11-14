9c8a2b6c980b6e1a97360d8551c1abb91f1f0ad6
Sutan Nst <sutan.gnst@gmail.com>
2018-11-14 19:10:19 +0700

    change background color in footer

--- views/desktop/includes/footer.php
+++ views/desktop/includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="page-footer white">
   <?php if (requesturl() == baseurl().'/') include_once 'sponsor.php';?>
-  <div class="footer-copyright deep-purple darken-3">
+  <div class="footer-copyright background--mind_3">
     <div class="container">
       &copy; TIM IT - B 2018
     </div>
